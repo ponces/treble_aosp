174f5c0b89ce98e373fb3773a8e58c5ee886f5f5
Alberto Ponces <ponces26@gmail.com>
2021-10-16 17:25:13 +0100

    Minor fix

--- apply-patches.sh
+++ apply-patches.sh
@@ -8,6 +8,7 @@ tree="$2"
 for project in $(cd $patches/patches/$tree; echo *);do
 	p="$(tr _ / <<<$project |sed -e 's;platform/;;g')"
 	[ "$p" == build ] && p=build/make
+	[ "$p" == treble/app ] && p=treble_app
 	[ "$p" == vendor/hardware/overlay ] && p=vendor/hardware_overlay
 	[ "$p" == packages/apps/settings ] && p=packages/apps/Settings
 	pushd $p
