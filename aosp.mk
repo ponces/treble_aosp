371317b5bd4b289ea0c1d34a71c896f879ff1198
Alberto Ponces <ponces26@gmail.com>
2023-02-23 13:44:26 +0000

    feat: Import of Face Unlock for T

--- aosp.mk
+++ aosp.mk
@@ -6,3 +6,12 @@ BOARD_EXT4_SHARE_DUP_BLOCKS := true
 PRODUCT_PACKAGES += \
     libaptX_encoder \
     libaptXHD_encoder
+
+PRODUCT_PACKAGES += \
+    FaceUnlockService
+
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.face_unlock_service.enabled=true
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.biometrics.face.xml
