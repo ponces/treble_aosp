a4b78c2c75569da3a7dbef8ca7375dda4104804a
Alberto Ponces <ponces26@gmail.com>
2023-03-28 14:48:30 +0100

    manifest: Import QcRilAm

--- apply-patches.sh
+++ apply-patches.sh
@@ -6,13 +6,13 @@ patches="$(readlink -f -- $1)"
 tree="$2"
 
 for project in $(cd $patches/patches/$tree; echo *); do
-	p="$(tr _ / <<<$project |sed -e 's;platform/;;g')"
-	[ "$p" == build ] && p=build/make
-	[ "$p" == treble/app ] && p=treble_app
-	[ "$p" == vendor/hardware/overlay ] && p=vendor/hardware_overlay
-	pushd $p &>/dev/null
-	for patch in $patches/patches/$tree/$project/*.patch; do
-		git am $patch || exit
-	done
-	popd &>/dev/null
+    p="$(tr _ / <<<$project |sed -e 's;platform/;;g')"
+    [ "$p" == build ] && p=build/make
+    [ "$p" == treble/app ] && p=treble_app
+    [ "$p" == vendor/hardware/overlay ] && p=vendor/hardware_overlay
+    pushd $p &>/dev/null
+    for patch in $patches/patches/$tree/$project/*.patch; do
+        git am $patch || exit
+    done
+    popd &>/dev/null
 done
