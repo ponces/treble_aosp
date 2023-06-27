fa50b3c6e76204415f5f5992f6ec5a33144874ac
Alberto Ponces <ponces26@gmail.com>
2023-06-28 00:10:48 +0100

    bump: Up to android-13.0.0_r59

--- build.sh
+++ build.sh
@@ -16,7 +16,7 @@ BD=$HOME/builds
 initRepos() {
     if [ ! -d .repo ]; then
         echo "--> Initializing workspace"
-        repo init -u https://android.googlesource.com/platform/manifest -b android-13.0.0_r52
+        repo init -u https://android.googlesource.com/platform/manifest -b android-13.0.0_r59
         echo
 
         echo "--> Preparing local manifest"
