74d76547d2859b151ce8258abdfe27d634b833ff
Alberto Ponces <ponces26@gmail.com>
2023-10-17 14:17:28 +0000

    feat: Include ThemePicker and ThemesStub

--- aosp.mk
+++ aosp.mk
@@ -1,4 +1,8 @@
 $(call inherit-product, device/phh/treble/fonts/fonts.mk)
 
+PRODUCT_PACKAGES += \
+    ThemePicker \
+    ThemesStub
+
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.system.ota.json_url=https://raw.githubusercontent.com/ponces/treble_build_aosp/android-14.0/ota.json
