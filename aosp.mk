45a14045ab438cfe638d9523b73244429cb0c500
Alberto Ponces <ponces26@gmail.com>
2023-10-17 09:16:07 +0000

    fix: BOARD_EXT4_SHARE_DUP_BLOCKS is already true on AOSP

--- aosp.mk
+++ aosp.mk
@@ -1,6 +1,4 @@
 $(call inherit-product, device/phh/treble/fonts/fonts.mk)
 
-BOARD_EXT4_SHARE_DUP_BLOCKS := true
-
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.system.ota.json_url=https://raw.githubusercontent.com/ponces/treble_build_aosp/android-14.0/ota.json
