6d89787afb1e9a48f923532bf6f3706370fda43a
Alberto Ponces <ponces26@gmail.com>
2023-10-29 23:46:55 +0000

    feat: Update ThemePicker to also handle live wallpapers

--- aosp.mk
+++ aosp.mk
@@ -1,8 +1,7 @@
 $(call inherit-product, device/phh/treble/fonts/fonts.mk)
 
 PRODUCT_PACKAGES += \
-    ThemePicker \
-    ThemesStub
+    ThemePicker
 
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.system.ota.json_url=https://raw.githubusercontent.com/ponces/treble_build_aosp/android-14.0/ota.json
