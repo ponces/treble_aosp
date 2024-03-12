411b2da4c60072dfb1ad7096984a796529fc81c2
Alberto Ponces <ponces26@gmail.com>
2024-03-12 15:11:14 +0000

    fix: give sync a second opportunity to redeem itself

--- build.sh
+++ build.sh
@@ -29,7 +29,7 @@ initRepos() {
 
 syncRepos() {
     echo "--> Syncing repos"
-    repo sync -c --force-sync --no-clone-bundle --no-tags -j$(nproc --all)
+    repo sync -c --force-sync --no-clone-bundle --no-tags -j$(nproc --all) || repo sync -c --force-sync --no-clone-bundle --no-tags -j$(nproc --all)
     echo
 }
 
