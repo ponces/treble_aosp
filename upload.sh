f0fb2fb4ff818ff5841b2299694bb7da9acc9a29
Bruno-chanrio <abcermelo84@gmail.com>
2024-05-22 15:48:34 -0300

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -13,8 +13,8 @@ set -e
 BL=$PWD/treble_aosp
 BD=$HOME/builds
 TAG="$(date +v%Y.%m.%d)"
-GUSER="ponces"
-GREPO="treble_aosp"
+GUSER="brunochanrio"
+GREPO="ChanrioDroid"
 
 createRelease() {
     echo "--> Creating release $TAG"
