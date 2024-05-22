d82f8a11ca65c88ddbe41f02c3feb4c24de8db40
Bruno-chanrio <abcermelo84@gmail.com>
2024-05-22 15:55:35 -0300

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -15,6 +15,7 @@ BD=$HOME/builds
 TAG="$(date +v%Y.%m.%d)"
 GUSER="brunochanrio"
 GREPO="ChanrioDroid"
+GITHUB_API_TOKEN="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"
 
 createRelease() {
     echo "--> Creating release $TAG"
